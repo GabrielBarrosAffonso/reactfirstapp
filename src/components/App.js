1f64b406ef57c21e43350115bc0462570b40ef7c
Gabriel Barros Affonso <gabriel.barros@vtex.com.br>
2019-11-20 21:18:05 -0300

    Primeira versão da App, Esqueleto do React

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post'
+
+const App = () => {
+    return(
+    <>
+        <h1>Votação Game of the Year</h1>
+        <Post title='Death Stranding'/>
+        <Post title='Sekiro'/>
+        <Post title='Control'/>
+    </>
+    );
+}
+
+export default App
